1fb522ff7fe510feac914f71cc22ea8828ef7200
DimitriDoo <dimitri.baldacchino@gmail.com>
2024-08-25 19:21:38 -0400

    Added a WallpaperManager project

--- WebViewWindow/WallpaperWindow.xaml.cs
+++ WebViewWindow/WallpaperWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
+using WallpaperManager;
 
 namespace WebViewWindow
 {
@@ -10,51 +11,6 @@ namespace WebViewWindow
     /// </summary>
     public partial class MainWindow : Window
     {
-        #region"DLL Imports"
-        
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, IntPtr windowTitle);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool SendMessageTimeout(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam, SendMessageTimeoutFlags fuFlags, uint uTimeout, out IntPtr lpdwResult);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-
-
-        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
-        private const int SPI_SETDESKWALLPAPER = 20;
-        private const int SPIF_UPDATEINIFILE = 1;
-        private const int SPIF_SENDCHANGE = 2;
-
-        private const uint SWP_NOSIZE = 0x0001;
-        private const uint SWP_NOMOVE = 0x0002;
-        private const uint WM_CLOSE = 0x0010;
-        private IntPtr _workerw;
-
-        private enum SendMessageTimeoutFlags : uint
-        {
-            SMTO_NORMAL = 0x0000,
-            SMTO_BLOCK = 0x0001,
-            SMTO_ABORTIFHUNG = 0x0002,
-            SMTO_NOTIMEOUTIFNOTHUNG = 0x0008
-        }
-        
-        #endregion
-
         public MainWindow()
         {
             InitializeComponent();
@@ -84,29 +40,7 @@ namespace WebViewWindow
         
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            IntPtr hwnd = new WindowInteropHelper(this).Handle;
-            IntPtr progman = FindWindow("Progman", null);
-            _workerw = IntPtr.Zero;
-
-            // Send a message to   to spawn a WorkerW
-            SendMessageTimeout(progman, 0x052C, IntPtr.Zero, IntPtr.Zero, SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out _);
-
-            // Find the WorkerW window
-            EnumWindows((tophandle, topparamhandle) =>
-            {
-                IntPtr p = FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", IntPtr.Zero);
-                if (p != IntPtr.Zero)
-                {
-                    _workerw = FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", IntPtr.Zero);
-                }
-                return true;
-            }, IntPtr.Zero);
-
-            // Set the parent of the WPF window to the WorkerW window
-            if (_workerw != IntPtr.Zero)
-            {
-                SetParent(hwnd, _workerw);
-            }
+            Manager.SetWallPaper(new WindowInteropHelper(this).Handle);
         }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
