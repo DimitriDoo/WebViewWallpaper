cbe9e2b3fff29fe8ba80e66523b78a1c9db1a92a
DimitriDoo <dimitri.baldacchino@gmail.com>
2024-08-25 18:37:40 -0400

    Created a basic selection window that follows the MVVM architecture

---
+++ WebViewWindow/View/WallpaperSelectionWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.IO;
+using WebViewWallpaper.ViewModels;
+
+namespace WebViewWallpaper
+{
+    /// <summary>
+    /// Interaction logic for WallpaperSelectionWindow.xaml
+    /// </summary>
+    public partial class WallpaperSelectionWindow : Window
+    {
+        public WallpaperSelectionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WebViewWindow/ViewModels/WallpaperDirectoriesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebViewWallpaper.Models;
+
+namespace WebViewWallpaper.ViewModels
+{
+    public class WallpaperDirectoriesViewModel
+    {
+        public WallpaperDirectoriesViewModel() 
+        {
+            List<WallpaperDirectory> dirs = new List<WallpaperDirectory>();
+            foreach (string dir in Directory.GetDirectories($@"{AppContext.BaseDirectory}\Wallpapers\"))
+            {
+                dirs.Add(new WallpaperDirectory() { Wallpaper = dir });
+            }
+            this.WallpaperDirectories = new ObservableCollection<WallpaperDirectory>(dirs);
+        }
+        public ObservableCollection<WallpaperDirectory> WallpaperDirectories { get; set; }
+    }
+}
